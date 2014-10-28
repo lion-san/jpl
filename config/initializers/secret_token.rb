05be8e02d81aa50b6da3704e86b6f100bf09b4b2
yokoi.shinya <shinya.yokoi@gmail.com>
2014-10-28 10:05:26 +0900

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Jpl::Application.config.secret_key_base = '2caa426e48d148080a96ca3c4849eeea6350027b3f47e2e7e75768b5150f4b358d890430303b9208c4a11966d99de2b93380f479d275a0644591c98596a583c0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Jpl::Application.config.session_store :cookie_store, key: '_jpl_session'
